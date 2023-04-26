5e11c8d18851907b10ad62fdd1e986e7a3e04375
ghshin-a <singahyeon12@naver.com>
2023-04-26 16:06:31 +0900

    noticeline 만드는 중

--- starbucks/tailwind.config.js
+++ starbucks/tailwind.config.js
@@ -15,11 +15,28 @@ module.exports = {
         screen2: { min: "641px", max: "1120px" },
         screen3: { max: "1400px" },
         screen4: { max: "961px" },
+        screen5: { min: "960px", max: "1340px" },
+        screen6: { min: "960px", max: "1479px" },
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        show: {
+          from: { opacity: 0 },
+          to: {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        show1: "show 500ms ease forwards",
+        show2: "show 500ms ease 500ms forwards",
+        show3: "show 500ms ease 1s forwards",
+        show4: "show 500ms ease 1500ms forwards",
+        show5: "show 500ms ease 2s forwards",
       },
     },
   },
